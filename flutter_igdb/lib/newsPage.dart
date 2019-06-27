14f34630a623f35ab8676ff15bbe8dbc87e2a9dc
Nicolas FAVRE <nicolas.favre@niji.fr>
2019-06-27 10:21:40 +0200

    add auto scroll on articles pageView

--- flutter_igdb/lib/newsPage.dart
+++ flutter_igdb/lib/newsPage.dart
@@ -3,6 +3,7 @@ import 'package:flutter_igdb/models/pulses.dart';
 import 'package:flutter_igdb/services/services.dart';
 import 'package:date_format/date_format.dart';
 import 'package:flutter_igdb/utils.dart';
+import 'dart:async';
 
 class NewsPage extends StatefulWidget {
   NewsPage({Key key}) : super(key: key);
@@ -13,9 +14,32 @@ class NewsPage extends StatefulWidget {
 
 class _NewsPageState extends State<NewsPage> {
   List<Pulses> articles;
+  int initialPage = 0;
+  int realPage = 10000;
+  Timer timer;
+  PageController pageController = PageController(
+    viewportFraction: 1.0,
+    initialPage: 0
+  );
+
   @override
   void initState() {
     super.initState();
+
+    timer = getTimer();
+  }
+
+  Timer getTimer() {
+    return Timer.periodic(Duration(seconds: 4), (_) {
+      pageController.nextPage(duration: Duration(milliseconds: 800), curve: Curves.fastOutSlowIn);
+    });
+  }
+
+  void pauseOnTouch() {
+    timer.cancel();
+    timer = Timer(Duration(seconds: 3), () {
+      timer = getTimer();
+    });
   }
 
   Widget build(BuildContext context) {
@@ -56,45 +80,49 @@ class _NewsPageState extends State<NewsPage> {
   }
 
   Widget _buildArticles() {
-    return new PageView.builder(
-      itemCount: articles.length,
-      itemBuilder: (context, int){
-        return InkWell(
-          onTap: () => Utility.launchURL(articles[int].website.url, true),
-          child: Stack(
-            children: <Widget>[
-              Positioned.fill(
-                child: Image.network(
-                  articles[int].image,
-                  scale: 2.0,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Positioned(
-                left: 0.0,
-                right: 0.0,
-                height: 70,
-                bottom: 0.0,
-                child: Container(
-                  padding: EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0),
-                  color: Color.fromRGBO(0, 0, 0, 0.70),
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text(articles[int].title,
-                          style: TextStyle(color: Colors.white),),
-                        Text(_authorDate(articles[int]),
-                          style: TextStyle(color: Colors.grey),
-                          textAlign: TextAlign.left,),
-                      ]
+    return GestureDetector(
+      onTapDown: (_) => pauseOnTouch(),
+      child: new PageView.builder(
+        controller: pageController,
+        itemCount: articles.length,
+        itemBuilder: (context, int){
+          return InkWell(
+            onTap: () => Utility.launchURL(articles[int].website.url, true),
+            child: Stack(
+              children: <Widget>[
+                Positioned.fill(
+                  child: Image.network(
+                    articles[int].image,
+                    scale: 2.0,
+                    fit: BoxFit.cover,
                   ),
                 ),
-              ),
-            ],
-          ),
-        );
-      },
+                Positioned(
+                  left: 0.0,
+                  right: 0.0,
+                  height: 70,
+                  bottom: 0.0,
+                  child: Container(
+                    padding: EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0),
+                    color: Color.fromRGBO(0, 0, 0, 0.70),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(articles[int].title,
+                            style: TextStyle(color: Colors.white),),
+                          Text(_authorDate(articles[int]),
+                            style: TextStyle(color: Colors.grey),
+                            textAlign: TextAlign.left,),
+                        ]
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 
